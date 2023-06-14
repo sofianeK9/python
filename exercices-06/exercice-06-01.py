d5bd23ae8f4e87b92dff7372c82ae68d944a5976
sofianek9 <kayasofiane7@gmail.com>
2023-06-14 11:34:58 +0200

    exercice 6.01

---
+++ exercices-06/exercice-06-01.py
@@ -0,0 +1,9 @@
+# exo 6.1
+# Créez une liste nommée `my_list` contenant un nombre entier, un nombre à virgule flottante, une chaîne de caractères et un booléen
+
+# réponse 6.1
+
+my_list = ["2", "1.2", "Sofiane", "True"]
+print(my_list)
+
+print(my_list.sort())
