ada4f83fde5e8d1aff8bac6e1c1d42ad769bfd3c
sofianek9 <kayasofiane7@gmail.com>
2023-06-21 18:29:14 +0200

    exercice 7.03

---
+++ exercices-07/exercice-07-03.py
@@ -0,0 +1,14 @@
+# exo 7.3
+# en utilisant une boucle for, on tire 100 fois un nombre entier `r` au hasard entre 1 et 10 inclus
+# affichez `r` s'il est égal à 1
+
+import random
+
+# réponse 7.3
+r = random.randint(1, 10)
+
+for i  in range (0,100):
+    r = random.randint(1, 10)
+    if r == 1:
+        print(r)
+ 
