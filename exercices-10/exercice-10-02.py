b84621fd4bdae02b12a7ab7dcbddf4c2434af06b
sofianek9 <kayasofiane7@gmail.com>
2023-06-26 15:42:27 +0200

    exercice 10.2

---
+++ exercices-10/exercice-10-02.py
@@ -0,0 +1,15 @@
+# exo 10.2
+# Créer une fonction nommée `my_diff()` qui :
+# - prend deux paramètres de type `int`
+# - soustrait `b` de `a`
+# - renvoie le résultat de type `int`
+# Notez bien le type hinting dans la déclaration de la fonction
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.2
+
+def my_diff(a: int, b: int):
+    return a - b
+
+resultat = my_diff(9, 6)
+print(resultat)
