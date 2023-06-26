18fe7a8ab69745d963756c7508255b49954076dc
sofianek9 <kayasofiane7@gmail.com>
2023-06-26 10:28:11 +0200

    exercice 10.1

---
+++ exercices-10/exercice-10-01.py
@@ -0,0 +1,17 @@
+# exo 10.1
+# Créer une fonction nommée `my_sum()` qui :
+# - prend deux paramètres
+# - additionne ces deux paramètres
+# - renvoie le résultat
+# Appelez la fonction et affichez le résultat
+
+# réponse 10.1
+
+
+def sum(a, b):
+    return a + b
+
+
+
+resultat = sum(12, 3)
+print(resultat)
