8e5d8f2be4d2b4186ff786363d2949fa88ce6bb6
sofianek9 <kayasofiane7@gmail.com>
2023-06-26 18:37:02 +0200

    exercice 10.6

---
+++ exercices-10/exercice-10-06.py
@@ -0,0 +1,36 @@
+# exo 10.6
+# La formule suivante permet de convertir des mètres en miles :
+#
+#       miles = meters / 1609.344
+#
+# La formule suivante permet de faire l'inverse :
+#
+#       meters = miles * 1609.344
+#
+# Créez une fonction nommée :
+#
+# - meters_to_miles() permettant de convertir des mètres en miles
+# - miles_to_meters() permettant de convertir des miles en mètres
+#
+# Ensuite convertissez les valeurs :
+#
+# - 1 Km en miles
+# - 10 miles en mètres
+#
+# Appelez les fonctions et affichez les résultats
+
+# réponse 10.6
+
+def meters_to_miles(a: float) -> float:
+    miles = a / 1609.344
+    return miles
+
+resultat = meters_to_miles(1000)
+print(round(resultat,2))
+    
+def miles_to_meters(b: float) -> float:
+    meters = b * 1609.344
+    return meters
+
+resultat = miles_to_meters(10)
+print(round(resultat,2))
